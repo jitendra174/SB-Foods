683a601d7bebf38fff7be469153d6528110cd8be
jitendrachinta <jitendrachinta34@gmail.com>
2025-06-25 19:21:36 +0530

    OKAY

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -33,7 +33,7 @@ export const signupUser = async (req, res) => {
   }
 
   try {
-    const existing = await User.findOne({ email });
+    const existing = await User.findOne({ email  });
     if (existing) {
       return res.status(400).json({ message: 'User already exists' });
     }
