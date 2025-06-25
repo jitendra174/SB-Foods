8601f509235f118cee1b8b818b0f493233711b52
jitendrachinta <jitendrachinta34@gmail.com>
2025-06-25 12:36:27 +0530

    Fix: added express.json() to enable login

--- server/server.js
+++ server/server.js
@@ -15,6 +15,7 @@ const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGO_URI;
 
 app.use(cors()); 
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('🟢 SB Foods API is running...');
